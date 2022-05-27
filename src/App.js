331a859a44340fa4a010cdabf5f2832e1ecb10b0
Sorawit Kwanmuk <sorawit_kwanmuk@outlook.com>
2022-05-27 14:24:57 +0700

    google map work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import './App.css';
 import { Loader } from '@googlemaps/js-api-loader';
 
 function App() {
-  return <div className='App'></div>;
+  // let map;
+
+  // const loader = new Loader({
+  //   apiKey: 'AIzaSyBQdM6-2q5JDY4Q4XWG3Dg1A5FWonZCDbc',
+  //   version: 'weekly',
+  // });
+
+  // loader.load().then(() => {
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: { lat: -34.397, lng: 150.644 },
+  //     zoom: 8,
+  //   });
+  // });
+
+  // function initMap() {
+
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: {
+  //       lat: -34.397,
+  //       lng: 150.644,
+  //     },
+  //     zoom: 8,
+  //   });
+  // }
+
+  // initMap();
+  return <div id='map'></div>;
 }
 
 export default App;
